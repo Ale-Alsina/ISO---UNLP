3fbd620174982d399ae35c2ddfe9bc284d62e485
Alejandro Alsina <83216208+Ale-Alsina@users.noreply.github.com>
2021-07-19 09:42:09 -0300

    Update ej17.sh

--- TP03/EJ17/ej17.sh
+++ TP03/EJ17/ej17.sh
@@ -8,21 +8,22 @@
 #                              iSo
 #                              PEPe
 #                              mRI
-function checkDir(){
-  if [ -d $ruta ]; then
-    ruta='./'$ruta;
-    cd './'$ruta;
-  else
+
+function checkDir(){          # funcion : verificar o crear el directorio para trabajar sobre el
+  if [ -d $ruta ]; then       # si existe el directorio entonces
+    ruta='./'$ruta;           # actualizar ubicacion del directorio
+    cd './'$ruta;             # entrar al directorio de trabajo
+  else                        #sino no existe
     mkdir $ruta;              # crear directorio
     echo "Se creara $ruta de prueba con archivos Iso, pepE y Maria";
-    cd './'$ruta;             # ingresar al directorio creado
-    touch IsO pepE Maria;     # agregar o crear archivos 
+    cd './'$ruta;                    # ingresar al directorio creado
+    touch IsO pepE Maria;            # agregar o crear archivos 
   fi  
 }
 
-ruta="DIR"; # simular un dir y crear 3 archivos "ponele el nom que quieras"
-checkDir;       # retorna con la ruta para ser tratado
-ls  | tr a-zA-Z A-Za-z | tr -d [aA];
+ruta="DIR";                          # simular un dir y crear 3 archivos "ponele el nom que quieras"
+checkDir;                            # retorna con la ruta para ser tratado
+ls  | tr a-zA-Z A-Za-z | tr -d [aA]; # tratar y mostrar resultado del la op
 # Donde 
 #    => 1ro tr -d [aA]       -> alimina tanto "a" como "A" de cadena c/nom arch
 #    => 2do tr a-zA-Z A-Za-z -> intercambio de mayusc a minusc y visceversa
